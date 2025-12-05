3b1ca6f6111d141d442a599214d03b9beafcd80b
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-05 23:11:35 +0530

    Refactor doWork method for MetadataSyncWorker

--- app/src/main/java/com/maazm7d/termuxhub/work/MetadataSyncWorker.kt
+++ app/src/main/java/com/maazm7d/termuxhub/work/MetadataSyncWorker.kt
@@ -5,13 +5,13 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.maazm7d.termuxhub.TermuxHubApp
 
-class MetadataSyncWorker(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
-    override suspend fun doWork(): Result {
-        return try {
-            val ok = TermuxHubApp.repository.refreshFromRemote()
-            if (ok) Result.success() else Result.retry()
-        } catch (e: Exception) {
-            Result.retry()
-        }
+class MetadataSyncWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    override suspend fun doWork(): Result = try {
+        val ok = TermuxHubApp.repository.refreshFromRemote()
+        if (ok) Result.success() else Result.retry()
+    } catch (e: Exception) {
+        Result.retry()
     }
-}
+    }
