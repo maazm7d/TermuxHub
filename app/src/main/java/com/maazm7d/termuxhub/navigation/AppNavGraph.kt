efc05531ea35165bc32caa1e65ab329565419b83
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-05 23:06:34 +0530

    Refactor AppNavHost parameters for readability

--- app/src/main/java/com/maazm7d/termuxhub/navigation/AppNavGraph.kt
+++ app/src/main/java/com/maazm7d/termuxhub/navigation/AppNavGraph.kt
@@ -17,6 +17,15 @@ import com.maazm7d.termuxhub.ui.screens.saved.SavedViewModel
 import com.maazm7d.termuxhub.ui.screens.settings.SettingsScreen
 import com.maazm7d.termuxhub.ui.screens.splash.SplashScreen
 
+object Destinations {
+    const val SPLASH = "splash"
+    const val HOME = "home"
+    const val DETAILS = "details"
+    const val SAVED = "saved"
+    const val BLOGS = "blogs"
+    const val SETTINGS = "settings"
+}
+
 @Composable
 fun TermuxHubAppNav(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
@@ -24,8 +33,15 @@ fun TermuxHubAppNav(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
-    NavHost(navController = navController, startDestination = Destinations.SPLASH, modifier = modifier) {
+fun AppNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Destinations.SPLASH,
+        modifier = modifier
+    ) {
 
         composable(Destinations.SPLASH) {
             SplashScreen(onFinished = {
