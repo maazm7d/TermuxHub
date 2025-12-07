6b446f71a90d8afd354b5adb31e93b5ac1807716
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-07 12:46:23 +0530

    Update CategoryChips.kt

--- app/src/main/java/com/maazm7d/termuxhub/ui/components/CategoryChips.kt
+++ app/src/main/java/com/maazm7d/termuxhub/ui/components/CategoryChips.kt
@@ -1,10 +1,9 @@
 package com.maazm7d.termuxhub.ui.components
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -12,7 +11,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun CategoryChips(
-    chips: List<String>,
+    chips: List<Pair<String, Int>>,
     selectedIndex: Int,
     onChipSelected: (Int) -> Unit
 ) {
@@ -20,11 +19,16 @@ fun CategoryChips(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)
     ) {
+
         itemsIndexed(chips) { index, item ->
+
+            val label = if (item.first == "All") "All (${item.second})"
+            else "${item.first} (${item.second})"
+
             FilterChip(
                 selected = selectedIndex == index,
                 onClick = { onChipSelected(index) },
-                label = { Text(item) },
+                label = { Text(label) },
                 shape = MaterialTheme.shapes.medium,
                 colors = FilterChipDefaults.filterChipColors(
                     selectedContainerColor = MaterialTheme.colorScheme.primary,
