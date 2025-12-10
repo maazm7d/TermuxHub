bbbce5bf8e5f03d5128a11547286f4523900a33e
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-10 12:28:26 +0530

    Update MetricsRow.kt

--- app/src/main/java/com/maazm7d/termuxhub/ui/components/MetricsRow.kt
+++ app/src/main/java/com/maazm7d/termuxhub/ui/components/MetricsRow.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MetricsRow(published: String) {
