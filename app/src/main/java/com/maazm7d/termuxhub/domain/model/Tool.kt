cfb819b3849bbf9a01a62cdc3e66ddd9825fd992
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-11 16:50:14 +0530

    Update Tool.kt

--- app/src/main/java/com/maazm7d/termuxhub/domain/model/Tool.kt
+++ app/src/main/java/com/maazm7d/termuxhub/domain/model/Tool.kt
@@ -1,4 +1,6 @@
 package com.maazm7d.termuxhub.domain.model
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class Tool(
     val id: String,
@@ -14,3 +16,11 @@ data class Tool(
     val publishedAt: String?,
     val tags: List<String> = emptyList()  // <-- Add this
 )
+
+fun Tool.getPublishedDate(): Date? {
+    return try {
+        SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH).parse(this.publishedAt ?: "")
+    } catch (e: Exception) {
+        null
+    }
+}
