069d5a299269bbb51cbd711743867166db45b500
maazm7d <skmaaz933628@gmail.com>
2025-12-13 21:46:06 +0530

    fix

--- app/src/main/java/com/maazm7d/termuxhub/ui/screens/blogs/BlogsScreen.kt
+++ app/src/main/java/com/maazm7d/termuxhub/ui/screens/blogs/BlogsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BlogsScreen() {
     Column {
