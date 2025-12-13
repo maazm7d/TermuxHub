8503376c9a22856540710a6aeeb82c0f5a67509a
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-13 15:02:13 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
     id("com.android.application") version "8.11.1" apply false
     id("com.android.library") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false  // ★ ADD THIS
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
 
 tasks.register<Delete>("clean") {
