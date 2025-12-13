72917c4b97d4dfcdfd8478f867c5eff21702f475
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-13 19:07:02 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.library") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
-    id("com.google.devtools.ksp") version "2.2.21-1.0.20" apply false
+    id("com.google.devtools.ksp") version "2.2.21-RC-2.0.4" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
 
