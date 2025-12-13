b0ec66ba004aea4bdf96032edd344df6865c2d30
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-13 10:42:25 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.maazm7d.termuxhub"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
