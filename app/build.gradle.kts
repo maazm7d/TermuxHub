7372a6088f4fd4fb202c8c7bbbc79ee2bccde3d0
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-14 21:21:41 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,10 +91,8 @@ implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
 ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 
 /* -------------------- Markdown (Compose compatible) -------------------- */
-implementation("com.mikepenz:multiplatform-markdown-renderer:0.38.1")
-implementation("com.mikepenz:multiplatform-markdown-renderer-m3:0.38.1")
-implementation("com.mikepenz:multiplatform-markdown-renderer-code:0.38.1")
-implementation("com.mikepenz:multiplatform-markdown-renderer-coil3:0.38.1")
+implementation("com.halilibo.compose-richtext:richtext-commonmark:0.17.0")
+implementation("com.halilibo.compose-richtext:richtext-ui-material3:0.17.0")
 
 /* -------------------- Image Loading -------------------- */
 implementation("io.coil-kt:coil-compose:2.6.0")
