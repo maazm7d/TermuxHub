b5f846c8a5bdedb419c76cb3a6bae24451cdaf5a
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-14 19:29:40 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,8 @@ plugins {
 id("com.android.application") 
 id("org.jetbrains.kotlin.android") 
 id("org.jetbrains.kotlin.plugin.compose") 
-id("com.google.devtools.ksp") id("kotlin-parcelize") 
+id("com.google.devtools.ksp") 
+id("kotlin-parcelize") 
 id("com.google.dagger.hilt.android") 
 }
 
