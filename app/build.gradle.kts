8f302f5c1bd3234cbedaf90a73d48d714fa20635
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-14 13:39:54 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material:1.6.7")
     implementation("com.google.android.material:material:1.13.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
