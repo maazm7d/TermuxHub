e036b4b98b564a63161c6d9620eb6664f5625a53
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-14 22:18:57 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,8 +91,10 @@ implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
 ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 
 /* -------------------- Markdown (Compose compatible) -------------------- */
-implementation("com.halilibo.compose-richtext:richtext-commonmark:0.17.0")
+// Compose RichText (Markdown rendering)
 implementation("com.halilibo.compose-richtext:richtext-ui-material3:0.17.0")
+implementation("com.halilibo.compose-richtext:richtext-commonmark:0.17.0")
+implementation("com.halilibo.compose-richtext:richtext-ui:0.17.0")
 
 /* -------------------- Image Loading -------------------- */
 implementation("io.coil-kt:coil-compose:2.6.0")
