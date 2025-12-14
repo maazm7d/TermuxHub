37f11ff44d73f710d2ab5c0d78e5a24bf1e1b798
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-14 19:40:30 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ debugImplementation("androidx.compose.ui:ui-tooling")
 
 implementation("androidx.navigation:navigation-compose:2.8.0")
 implementation("com.google.android.material:material:1.13.0")
+implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
 
 /* -------------------- Lifecycle -------------------- */
 implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
