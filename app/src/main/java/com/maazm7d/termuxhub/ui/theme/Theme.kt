a5bfb4b798444b877e1f096333e7e82a22137f57
maazm7d <skmaaz933628@gmail.com>
2025-12-27 17:42:23 +0530

    Revert "Update Theme.kt"
    This reverts commit b4b0be6568ae65b2ee137e7275ffdd6b357bae3b.

--- app/src/main/java/com/maazm7d/termuxhub/ui/theme/Theme.kt
+++ app/src/main/java/com/maazm7d/termuxhub/ui/theme/Theme.kt
@@ -7,20 +7,20 @@ import androidx.compose.runtime.Composable
 @Composable
 fun TermuxHubTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colorScheme = lightColorScheme(
+        colorScheme = androidx.compose.material3.lightColorScheme(
             primary = md_theme_light_primary,
             onPrimary = md_theme_light_onPrimary,
             background = md_theme_light_background,
-            surface = md_theme_light_surface
+            surface = md_theme_light_surface,
+            onSurface = md_theme_light_onSurface,
+            outline = md_theme_light_outline
         ),
         typography = Typography,
-        shapes = Shapes
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            content()
+        shapes = Shapes,
+        content = {
+            Surface {
+                content()
+            }
         }
-    }
+    )
 }
