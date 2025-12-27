9e0b05c32fc55b25314ff38dadfde1ef731fbb18
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-27 10:43:11 +0530

    Update Theme.kt

--- app/src/main/java/com/maazm7d/termuxhub/ui/theme/Theme.kt
+++ app/src/main/java/com/maazm7d/termuxhub/ui/theme/Theme.kt
@@ -3,6 +3,9 @@ package com.maazm7d.termuxhub.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxSize
 
 @Composable
 fun TermuxHubTheme(content: @Composable () -> Unit) {
