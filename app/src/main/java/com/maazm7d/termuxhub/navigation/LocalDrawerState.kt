11b9708198e2d7c3ed69d02dad6eac6b0b794a74
maazm7d <skmaaz933628@gmail.com>
2025-12-27 17:42:19 +0530

    Revert "Create LocalDrawerState.kt"
    This reverts commit f7aaac90b5355f19fdbdb1fe4de2fbce29b8a4c2.

--- app/src/main/java/com/maazm7d/termuxhub/navigation/LocalDrawerState.kt
+++
@@ -1,8 +0,0 @@
-package com.maazm7d.termuxhub.navigation
-
-import androidx.compose.material3.DrawerState
-import androidx.compose.runtime.staticCompositionLocalOf
-
-val LocalDrawerState = staticCompositionLocalOf<DrawerState> {
-    error("DrawerState not provided")
-}
