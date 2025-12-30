def2cc39b487c0bae5f7d6a8a74a798e82a9b983
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-12-30 12:54:28 +0530

    Update AppDatabase.kt

--- app/src/main/java/com/maazm7d/termuxhub/data/local/AppDatabase.kt
+++ app/src/main/java/com/maazm7d/termuxhub/data/local/AppDatabase.kt
@@ -5,10 +5,20 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.maazm7d.termuxhub.data.local.entities.ToolEntity
+import com.maazm7d.termuxhub.data.local.entities.HallOfFameEntity
 
-@Database(entities = [ToolEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        ToolEntity::class,
+        HallOfFameEntity::class
+    ],
+    version = 2,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun toolDao(): ToolDao
+    abstract fun hallOfFameDao(): HallOfFameDao
 
     companion object {
         @Volatile
@@ -16,9 +26,14 @@ abstract class AppDatabase : RoomDatabase() {
 
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
-                val inst = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "termuxhub_db")
+                val inst = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "termuxhub_db"
+                )
                     .fallbackToDestructiveMigration()
                     .build()
+
                 INSTANCE = inst
                 inst
             }
