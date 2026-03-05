0cf0fcaf5eb320054152502cdea3767d389bbcf6
M A A Z <104779758+maazm7d@users.noreply.github.com>
2026-03-06 01:03:50 +0530

    Refactor SearchBar component for improved clarity

--- app/src/main/java/com/maazm7d/termuxhub/ui/components/SearchBar.kt
+++ app/src/main/java/com/maazm7d/termuxhub/ui/components/SearchBar.kt
@@ -1,18 +1,22 @@
 package com.maazm7d.termuxhub.ui.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
     queryState: MutableState<String>,
@@ -22,8 +26,7 @@ fun SearchBar(
     TextField(
         value = queryState.value,
         onValueChange = { queryState.value = it },
-        modifier = modifier
-            .fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         placeholder = {
             Text(
                 text = placeholder,
@@ -54,7 +57,6 @@ fun SearchBar(
             focusedContainerColor = MaterialTheme.colorScheme.surface,
             unfocusedContainerColor = MaterialTheme.colorScheme.surface,
             disabledContainerColor = MaterialTheme.colorScheme.surface,
-            errorContainerColor = MaterialTheme.colorScheme.surface,
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent,
             cursorColor = MaterialTheme.colorScheme.primary
